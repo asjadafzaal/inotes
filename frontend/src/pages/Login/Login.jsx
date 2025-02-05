dc702252554241665661be399540d44771e82f4a
Asjad Afzaal <132608659+asjad491@users.noreply.github.com>
2025-02-05 13:06:37 -0500

    save

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
                 <div className="mb-8">
                   <h3 className="text-gray-800 text-3xl font-extrabold">Sign in</h3>
                   <p className="text-gray-500 text-sm mt-4 leading-relaxed">
-                    Sign in to your account and explore a world of possibilities. Your journey begins here.
+                    Sign in to your account and explore a world of possibilities. Your journey begins here. asjad
                   </p>
                 </div>
 
