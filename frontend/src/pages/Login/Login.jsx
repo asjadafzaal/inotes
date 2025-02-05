9f068f32bb46cc88ca8a4853f9319fcbd429634f
Asjad Afzaal <132608659+asjad491@users.noreply.github.com>
2025-02-05 13:14:42 -0500

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -117,7 +117,7 @@ const Login = () => {
               transition={{ duration: 1 }}
             >
               <img
-                src='public/assets/loginimg.png'
+                src='/public/assets/loginimg.png'
                 className="w-full h-full max-md:w-4/5 mx-auto block object-cover"
                 alt="Dining Experience"
               />
